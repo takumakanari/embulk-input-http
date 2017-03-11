3cd6bbd5ac0dc81f108fbaa42097519371858c21
takumakanari <chemtrails.t@gmail.com>
2017-03-11 15:37:46 +0900

    fix styles

--- src/main/java/org/embulk/input/http/BasicAuthConfig.java
+++ src/main/java/org/embulk/input/http/BasicAuthConfig.java
@@ -4,36 +4,40 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Objects;
 
-public class BasicAuthConfig {
-
+public class BasicAuthConfig
+{
     private final String user;
     private final String password;
 
     @JsonCreator
-    public BasicAuthConfig(
-            @JsonProperty("user") String user,
-            @JsonProperty("password") String password) {
+    public BasicAuthConfig(@JsonProperty("user") String user,
+            @JsonProperty("password") String password)
+    {
         this.user = user;
         this.password = password;
     }
 
     @JsonProperty("user")
-    public String getUser() {
+    public String getUser()
+    {
         return user;
     }
 
     @JsonProperty("password")
-    public String getPassword() {
+    public String getPassword()
+    {
         return password;
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode()
+    {
         return Objects.hashCode(user, password);
     }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return String.format("BasicAuthConfig[%s, %s]", getUser(), getPassword());
     }
 }
